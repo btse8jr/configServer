c185162dbf3e88f900ebae86b6ddcb2c4fac0391
btse8jr <chinai19610310@126.com>
2016-10-24 13:44:52 +0800

    配置服务与服务中心同级，不应注册

--- src/main/java/com/jj/Application.java
+++ src/main/java/com/jj/Application.java
@@ -3,11 +3,13 @@ package com.jj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by qmf on 2016/4/25.
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
